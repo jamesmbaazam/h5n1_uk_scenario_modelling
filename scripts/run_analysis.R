c8df7c61314f3488c756a2983d9905ff8ab81eaf
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-04-11 15:43:18 +0100

    remove scripts that are not used in manuscript

--- scripts/run_analysis.R
+++
@@ -1,7 +0,0 @@
-require("here")
-
-#Source analysis scripts
-source(here("scripts","load_packages.R"))
-source(here("scripts","load_params.R"))
-source(here("scripts","testing_model.R"))
-source(here("scripts","results.R"))
